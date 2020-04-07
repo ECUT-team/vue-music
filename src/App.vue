53868f97d86fe197d6f34dc28da656f50f8e5668
zby-66 <1753058819@qq.com>
2020-04-07 09:42:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Login></Login>
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login/Login";
+export default {
+  components: {
+    Login
+  }
+};
+</script>
+
+<style>
+</style>
